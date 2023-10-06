277d9cc9390b7d762e7d9abd59128acea8c7f584
Tejas <67954224+tejas-git64@users.noreply.github.com>
2023-10-06 22:32:23 +0530

    Update Firebase.ts

--- src/auth/Firebase.ts
+++ src/auth/Firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCIoSHIK9oce5iowlrrEG0qVVD9XhY8mH8",
-	authDomain: "bookdepo-store.firebaseapp.com",
-	projectId: "bookdepo-store",
-	storageBucket: "bookdepo-store.appspot.com",
-	messagingSenderId: "905097152156",
-	appId: "1:905097152156:web:16eab3c318bfbebcdb4f59",
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
 };
 
 // Initialize Firebase
