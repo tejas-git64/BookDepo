596af81b60c015d836b2e40e2054a4fbb32fa15e
tejas-git64 <tejas0586@gmail.com>
2023-10-06 22:18:13 +0530

    added sessionstorage for data persistence

--- src/auth/Firebase.ts
+++ src/auth/Firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCIoSHIK9oce5iowlrrEG0qVVD9XhY8mH8",
-	authDomain: "bookdepo-store.firebaseapp.com",
-	projectId: "bookdepo-store",
-	storageBucket: "bookdepo-store.appspot.com",
-	messagingSenderId: "905097152156",
-	appId: "1:905097152156:web:16eab3c318bfbebcdb4f59",
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
 };
 
 // Initialize Firebase
@@ -18,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
